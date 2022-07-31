4b86e3429d45578857beb321619318603f183346
jake484 <522432938@qq.com>
2022-07-31 11:45:54 +0800

    change to heatlab

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# AppsOnHeroku
-Deploy Genie Apps on Heroku 
+# Ai4Energy-HeatLab
+
+This branch is created to deploy [HeatLab Apps](https://ai4energy-heatlab.herokuapp.com/) on Heroku.
+
+It will take a long to start the app when you open it for the first time, which is determined by Heroku.
+
+It's worth your patience as it's **free** to deploy.🤣🤣🤣
+
+See the introduction of apps in [Docs](https://ai4energy.github.io/Ai4ELab/dev/labs/HeatLab/).
--- lib/MyApp.jl
+++ lib/MyApp.jl
@@ -1,44 +1,61 @@
-using Stipple, StipplePlotly, StippleUI
+using Stipple, StipplePlotly, StippleUI, Genie
+using DataFrames
+
+include("solver.jl") # get_data from solver.jl
 
 module MyApp
 using Stipple,StipplePlotly, StippleUI, DataFrames
 @reactive mutable struct MyPage <: ReactiveModel
+    tableData::R{DataTable} = DataTable(DataFrame(1000 * ones(10, 10), ["$i" for i in 1:10]))
+    credit_data_pagination::DataTablePagination = DataTablePagination(rows_per_page=10)
+
+
+    
     value::R{Int} = 0
     click::R{Int} = 0
 
-    features::R{Vector{Symbol}} = [:sin, :cos, :log, :tanh]
-    f_left::R{Symbol} = :sin
-    f_right::R{Symbol} = :sin
+    func_features::R{Vector{Symbol}} = [:_sin, :_tanh, :_sign]
+    func::R{Symbol} = :_sign
+
+    T0::R{Float64} = 1500.0
+    Tout::R{Float64} = 0.0
+    timefield::R{Float64} = 100
+    para::R{Float64} = 1.0
 
     plot_data::R{Vector{PlotData}} = []
     layout::R{PlotLayout} = PlotLayout(plot_bgcolor="#fff")
 
-    x_limit::R{Int} = 3
-    paramenter::R{Float32} = 1.2
-
 end
 end
 
 
-pd(f, para, xlim, name) = PlotData(
-    x=Float64[i for i in 1:0.1:xlim],
-    y=Float64[para * f(i) for i in 1:0.1:xlim],
-    plot=StipplePlotly.Charts.PLOT_TYPE_SCATTER,
-    name=name,
+contourPlot(z, n=10, L=0.2) = PlotData(
+    x=collect(range(0, L, length=n)),
+    y=collect(range(0, L, length=n)),
+    z=[z[:, i] for i in 1:10],
+    plot=StipplePlotly.Charts.PLOT_TYPE_CONTOUR,
+    contours=Dict("start" => 0, "end" => 1000),
+    name="test",
 )
 
 
 function compute_data(ic_model::MyApp.MyPage)
-    f_left = isequal(ic_model.f_left[], nothing) ? sin : eval(ic_model.f_left[])
-    f_right = isequal(ic_model.f_right[], nothing) ? sin : eval(ic_model.f_right[])
-    xlim = ic_model.x_limit[]
-    para = ic_model.paramenter[]
-    for i in 0:30
-        ic_model.plot_data[] = [pd(f_left, para, xlim + i, "测试函数1"), pd(f_right, para, xlim + i, "测试函数2")]
+    T0 = ic_model.T0[]
+    Tout = ic_model.Tout[]
+    timefield = ic_model.timefield[]
+    para = ic_model.para[]
+    func = ic_model.func[]
+    res = get_data(T0, Tout, timefield, para, func)
+    len = length(res[1, 1, :])
+    for i in 1:len
+        ic_model.plot_data[] = [contourPlot(res[:, :, i])]
+        ic_model.tableData[] = DataTable(
+            DataFrame(round.(res[:, :, i], digits=2), ["$i" for i in 1:10]))
         sleep(1 / 30)
     end
     nothing
 end
+
 
 function ui(model::MyApp.MyPage)
 
@@ -60,7 +77,7 @@ function ui(model::MyApp.MyPage)
             }
 
             .st-module {
-              marign: 10px;
+              marign: 20px;
               background-color: #FFF;
               border-radius: 5px;
               box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
@@ -73,67 +90,80 @@ function ui(model::MyApp.MyPage)
             """
         ),
         [
-            heading("测试")
+            heading("二维平板换热虚拟仿真实验室(Two Dimensional Plate Heat Transfer Virtual Simulation Laboratory)")
             row([
                 cell(
                     class="st-module",
                     [
-                        h6("X轴范围")
-                        slider(1:1:20,
-                            @data(:x_limit);
+                        h6("Initial Temperature: T0(℃)")
+                        slider(1000:50:2000,
+                            @data(:T0);
                             label=true)
                     ]
                 )
                 cell(
                     class="st-module",
                     [
-                        h6("函数系数")
-                        slider(1:0.1:2,
-                            @data(:paramenter);
+                        h6("Environmental Temperature: Tout(℃)")
+                        slider(0:50:500,
+                            @data(:Tout);
                             label=true)
                     ]
                 )
                 cell(
                     class="st-module",
                     [
-                        h6("测试函数1")
-                        Stipple.select(:f_left; options=:features)
+                        h6("Coefficient of t: Para")
+                        slider(0:0.1:2,
+                            @data(:para);
+                            label=true)
                     ]
                 )
                 cell(
                     class="st-module",
                     [
-                        h6("测试函数2")
-                        Stipple.select(:f_right; options=:features)
+                        h6("Time Domain(s)")
+                        slider(40:20:400,
+                            @data(:timefield);
+                            label=true)
+                    ]
+                )
+                cell(
+                    class="st-module",
+                    [
+                        h6("Change of Environmental Temperature")
+                        Stipple.select(:func; options=:func_features)
                     ]
                 )])
             row([
-                cell(
-                    class="st-module",
-                    p([
-                        "Simulation Times: "
-                        span(model.click, @text(:click))
-                    ])
-                )
                 btn("Simulation!", color="primary", textcolor="black", @click("value += 1"), [
                     tooltip(contentclass="bg-indigo", contentstyle="font-size: 16px",
-                        style="offset: 10px 10px", "点击按钮开始仿真!")])
-                cell(
-                    class="st-module",
-                    p([
-                        "Simulation Times: "
-                        span(model.click, @text(:click))
-                    ])
-                )])
-            row([
+                        style="offset: 10px 10px", "Click the button to start simulation")])
                 cell(
                     class="st-module",
                     [
-                        h5("仿真结果：")
+                        h6(["Simulation Times: ",
+                            span(model.click, @text(:click))])
+                    ])
+            ])
+            row([
+                cell(
+                    size=6,
+                    class="st-module",
+                    [
+                        h5("Result Plot")
                         plot(:plot_data, layout=:layout, config="{ displayLogo:false }")
                     ]
                 )
+                cell(
+                    class="st-module",
+                    [
+                        h5("Result Data")
+                        table(:tableData; pagination=:credit_data_pagination, label=false, flat=true)
+                    ]
+                )
             ])
-        ]
+            ]
+            
     )
 end
